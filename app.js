96f99d1fdc0cf32acf7a05c83a239a22c17ced91
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-05-23 14:00:58 -0400

    Refactor Scrolling On-Click to Be More Concise
    The decision was made to use a simpler scroll function (using scrollIntoView and behavior: smooth).
    
    The desired effect of smooth scrolling when a Navbar item is clicked is still achieved with this simplification, and the code becomes cleaner and more compact. Building out more customization of the scrolling manner in a function may still be desirable in the future, but for now, this far simpler smoothScroll function should work.

--- app.js
+++ app.js
@@ -68,113 +68,19 @@ for (let i = 1; i < (sections.length + 1); i++) {
     // been clicked.
     this.className += "active";
   });
-
-//  navListItem.id = i;
-/*Use the upward-counting local variable "i" to give each Navbar
-element a link reference to its corresponding section in the HTML.*/
-/*navListItem.innerHTML = `<a href=#section` + /*navListItem.id*//* i + `
-class="navbar__menu  menu__link">` + sections[i-1].dataset.nav +
-`</a>`;
-
-  navListItem.addEventListener("click", function() {
-    let current = document.getElementsByClassName("active");
-
-    //If there is no active class:
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-
-    //Add the active class to the current/clicked button
-    this.className += " active";
-  });*/
-
   }
 
 //Create scroll function taking into account a section's position.
 //For the moment, this is not a for loop or other kind of function
 //to iterate over sections, but rather just checks coordinates
 //of one section passed into the function.
-function smoothScroll(section, duration) {
-  console.log("smoothScroll starting");
-  console.log("Section is " + section);
-  let wordyword = section;
-  console.log(wordyword);
-  let scrollTarget = document.querySelector(wordyword);
+function smoothScroll(section) {
+  let clickedSection = section;
+  let scrollTarget = document.querySelector(clickedSection);
   scrollTarget.scrollIntoView({
     behavior: 'smooth'
   });
-  console.log(scrollTarget);
-  let targetPosition = scrollTarget.getBoundingClientRect().top;
-  //console.log("targetPosition " + targetPosition);
-  let startPosition = window.pageYOffset;
-  //console.log("startPosition " + startPosition);
-  let distance = targetPosition - startPosition;
-  //console.log("distance " + distance);
-  let startTime = null;
-  //console.log("startTime intentionally set to " + startTime);
-
-  /*function animation(currentTime) {
-    console.log("animation starting");
-    if (startTime === null) {
-      //console.log("startTime is indeed null");
-      startTime = currentTime;
-      //console.log("startTime is " + startTime);
-      let timeElapsed = currentTime - startTime;
-      //console.log("timeElapsed is instant: " + timeElapsed);
-      let yCoord = yCoordinate(timeElapsed, startPosition, distance, duration, targetPosition);
-      //console.log("The yCoord position is " + yCoord);
-      //console.log("yCoord is " + yCoord);
-      //console.log("I'm giving the yCoord to scrollTo()");
-      if(yCoord !== 0) {
-        setTimeout(function() {
-          console.log("yCoord is not 0, it is " + yCoord);
-          window.scrollTo(0, yCoord);
-          console.log("trying after timeout");
-        }, 250);
-        //console.log("The scrollTo should have just yCoord");
-        if (timeElapsed < duration) {
-          requestAnimationFrame(animation);
-        }
-      }
-      else if (yCoord === 0) {
-          console.log("yCoord is equal to 0");
-          setTimeout(function() {
-            window.scrollTo(0, yCoord);
-            console.log("trying after timeout");
-          }, 250);
-          //console.log("The scrollTo should have just yCoord");
-          if (timeElapsed < duration) {
-            requestAnimationFrame(animation);
-          }
-      }
-    }
-  }*/
-
-// The yCoordinate function returns the y-coordinate of the right
-// section that the page should scroll to when a Navbar item is
-// clicked.
-  function yCoordinate(t, b, c, d, e) {
-    console.log(t + " " + b + " " + c + " " + d);
-    console.log("yCoordinate starting");
-    // When the page sits at y = 0 (top of the page), simply jump
-    // to the desired y-coordinate which is "distance" away from 0.
-    if (b === 0) {
-      return c;
-    }
-    // When the page has been scrolled down at all, factor in a new
-    // startPosition to get the accurate y-coordinate of the target
-    else if (b > 0) {
-      console.log("non-zero y is " + b);
-      console.log("distance to target is " + c);
-      return (b+e);
-      }
-  }
-
-//  window.requestAnimationFrame(animation);
-  //console.log("targetPosition at bottom is " + targetPosition);
 }
-
-// Scroll to anchor ID using scrollTO event
 
 // Get all sections (by their anchor links) on the page
 const anchorLinks = document.querySelectorAll('a[href^="#"]');
@@ -190,13 +96,11 @@ anchorLinks.forEach(link => {
     // scrollTarget without actually scrolling
     event.preventDefault();
     //console.log("Clicked " + scrollTarget + " - preventDefault applied");
-    // Call the smoothScroll Function
-    //console.log("trying smoothScroll now:");
     //Remember, scrollTarget will be whichever Navbar Item is clicked
-    console.log('5/23 ' + scrollTarget);
-    smoothScroll(scrollTarget, 1000);
+    smoothScroll(scrollTarget);
   })
 })
+
 // Add class 'active' to section when near top of viewport
 
 
@@ -207,7 +111,3 @@ anchorLinks.forEach(link => {
 */
 
 // Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
